bec85073d3dbc7c5992d4628954573e210ee40ba
John Martin <john.itvn@gmail.com>
2015-07-23 21:42:01 +0700

    Update for rbac intergrate and fix ajaxcrud old version bug

--- src/basic/views/manager/_columns.php
+++ src/basic/views/manager/_columns.php
@@ -4,15 +4,11 @@ use yii\helpers\Url;
 use yii\helpers\Html;
 use yii\jui\DatePicker;
 
-return [
+$columns = [
     [
         'class' => 'kartik\grid\CheckboxColumn',
         'width' => '20px',
     ],
-//    [
-//        'class' => 'kartik\grid\SerialColumn',
-//        'width' => '30px',
-//    ],
     [
         'class' => '\kartik\grid\DataColumn',
         'attribute' => 'id',
@@ -90,48 +86,78 @@ return [
         },
         'format' => 'raw'
     ],
-    [
-        'class' => '\kartik\grid\DataColumn',
-        'width' => '130px',
-        'attribute' => 'administrator',
-        'value' => function ($model) {
-            if (!$model->administrator) {
-                return Html::a(Yii::t('user', 'Set SU'), ['toggle-superuser', 'id' => $model->id], [
-                            'class' => 'btn btn-xs btn-danger btn-block',
-                            'role' => 'modal-remote',
-                            'data-confirm' => false, 'data-method' => false, // for overide yii data api
-                            'data-request-method' => 'post',
-                            'data-confirm-title' => Yii::t('user', 'Are you sure?'),
-                            'data-confirm-message' => Yii::t('user', 'Are you sure you want to unblock this user?'),
-                ]);
-            } else {
-                return Html::a(Yii::t('user', 'Remove SU'), ['toggle-superuser', 'id' => $model->id], [
-                            'class' => 'btn btn-xs btn-info btn-block',
-                            'role' => 'modal-remote',
-                            'data-confirm' => false, 'data-method' => false, // for overide yii data api
-                            'data-request-method' => 'post',
-                            'data-confirm-title' => Yii::t('user', 'Are you sure?'),
-                            'data-confirm-message' => Yii::t('user', 'Are you sure you want to unblock this user?'),
-                ]);
-            }
-        },
-        'format' => 'raw',
-        'filter' => [0 => 'Not Admin', 1 => 'Admin'],
-    ],
-    [
-        'class' => 'kartik\grid\ActionColumn',
-        'dropdown' => false,
-        'vAlign' => 'middle',
-        'urlCreator' => function($action, $model, $key, $index) {
-            return Url::to([$action, 'id' => $key]);
-        },
-            'viewOptions' => ['role' => 'modal-remote', 'title' => 'View', 'data-toggle' => 'tooltip'],
-            'updateOptions' => ['role' => 'modal-remote', 'title' => 'Update', 'data-toggle' => 'tooltip'],
-            'deleteOptions' => ['role' => 'modal-remote', 'title' => 'Delete',
-                'data-confirm' => false, 'data-method' => false, // for overide yii data api
-                'data-request-method' => 'post',
-                'data-toggle' => 'tooltip',
-                'data-confirm-title' => 'Are you sure?',
-                'data-confirm-message' => 'Are you sure want to delete this item'],
-        ],
-    ];
+];
+  
+        
+$rbacModule = Yii::$app->getModule('rbac');
+        
+$columns[] =   [
+    'class' => '\kartik\grid\DataColumn',
+    'width' => '130px',
+    'attribute' => 'administrator',
+    'value' => function ($model) {
+        if (!$model->administrator) {
+            return Html::a(Yii::t('user', 'Set SU'), ['toggle-superuser', 'id' => $model->id], [
+                        'class' => 'btn btn-xs btn-danger btn-block',
+                        'role' => 'modal-remote',
+                        'data-confirm' => false, 'data-method' => false, // for overide yii data api
+                        'data-request-method' => 'post',
+                        'data-confirm-title' => Yii::t('user', 'Are you sure?'),
+                        'data-confirm-message' => Yii::t('user', 'Are you sure you want to unblock this user?'),
+            ]);
+        } else {
+            return Html::a(Yii::t('user', 'Remove SU'), ['toggle-superuser', 'id' => $model->id], [
+                        'class' => 'btn btn-xs btn-info btn-block',
+                        'role' => 'modal-remote',
+                        'data-confirm' => false, 'data-method' => false, // for overide yii data api
+                        'data-request-method' => 'post',
+                        'data-confirm-title' => Yii::t('user', 'Are you sure?'),
+                        'data-confirm-message' => Yii::t('user', 'Are you sure you want to unblock this user?'),
+            ]);
+        }
+    },
+    'format' => 'raw',
+    'filter' => [0 => 'Not Admin', 1 => 'Admin'],
+];
+    
+if (get_class($rbacModule) === 'johnitvn\rbacplus\Module') {
+    /**
+     * Intergrate with Rbac Plus extension
+     */
+    $columns[] = [
+            'class' => 'kartik\grid\DataColumn',
+            'header' => Yii::t('rbac', 'Assignment'),   
+            'hAlign' => 'center',
+            'value'=>function($model){
+                return Html::a('<span class="glyphicon glyphicon-king"></span>',
+                                ['/rbac/assignment/assignment', 'id' => $model->id], 
+                                [
+                                    'role' => 'modal-remote',
+                                    'title' => Yii::t('user', 'Assignment'),
+                                ]
+                        );
+            },
+            'format' => 'raw',    
+            'visible' => Yii::$app->user->identity->isAdministrator(),        
+        ];
+    
+}
+        
+$columns[] =   [
+    'class' => 'kartik\grid\ActionColumn',
+    'dropdown' => false,
+    'vAlign' => 'middle',
+    'urlCreator' => function($action, $model, $key, $index) {
+        return Url::to([$action, 'id' => $key]);
+    },
+        'viewOptions' => ['role' => 'modal-remote', 'title' => 'View', 'data-toggle' => 'tooltip'],
+        'updateOptions' => ['role' => 'modal-remote', 'title' => 'Update', 'data-toggle' => 'tooltip'],
+        'deleteOptions' => ['role' => 'modal-remote', 'title' => 'Delete',
+        'data-confirm' => false, 'data-method' => false, // for overide yii data api
+        'data-request-method' => 'post',
+        'data-toggle' => 'tooltip',
+        'data-confirm-title' => 'Are you sure?',
+        'data-confirm-message' => 'Are you sure want to delete this item'],
+];
+
+    return $columns;
